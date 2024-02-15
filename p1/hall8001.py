5f9e890c095ab555238f257907c667a37d8ab5cd
Gio <giomarchetti97@gmail.com>
2024-02-15 16:12:10 +0100

    creazione progetto p2

---
+++ p1/hall8001.py
@@ -0,0 +1,136 @@
+# Attenzione: versione di chatbot richiesta: 1.0.4
+# Attenzione: versione di python richiesta: 3.7.0
+
+# Importazione librerie:
+import re
+import time
+from chatterbot import ChatBot as CB
+from chatterbot.trainers import ListTrainer, ChatterBotCorpusTrainer
+
+#==============================================================
+
+# Definizioe funzione di pulizia del testo
+def pulizia_testo(testo):
+
+    # Impostazione testo in minuscolo
+    testo = testo.lower()
+    
+    testo = re.sub(r"i'm", "i am", testo)
+    testo = re.sub(r"he's", "he is", testo)
+    testo = re.sub(r"she's", "she is", testo)
+    testo = re.sub(r"it's", "it is", testo)
+    testo = re.sub(r"that's", "that is", testo)
+    testo = re.sub(r"what's", "that is", testo)
+    testo = re.sub(r"where's", "where is", testo)
+    testo = re.sub(r"how's", "how is", testo)
+    testo = re.sub(r"\'ll", " will", testo)
+    testo = re.sub(r"\'ve", " have", testo)
+    testo = re.sub(r"\'re", " are", testo)
+    testo = re.sub(r"\'d", " would", testo)
+    testo = re.sub(r"\'re", " are", testo)
+    testo = re.sub(r"won't", "will not", testo)
+    testo = re.sub(r"can't", "cannot", testo)
+    testo = re.sub(r"n't", " not", testo)
+    testo = re.sub(r"n'", "ng", testo)
+    testo = re.sub(r"'bout", "about", testo)
+    testo = re.sub(r"'til", "until", testo)
+    testo = re.sub(r"[-()\"#/@;:<>{}`+=~|.!?,]", "", testo)
+    
+    return testo
+
+# ============================================================
+
+# Definizione funzione di importazione testo
+# Caricamento file di testo
+linee = open('movie_lines.txt', encoding='utf-8', errors='ignore').read().split('\n')
+linee_conversazioni = open('movie_conversations.txt', encoding='utf-8', errors='ignore').read().split('\n')
+
+# Creazione di un dizionario che mappa l'ID di ogni linea di dialogo con il proprio testo
+id2line = {}
+for linea in linee:
+    _linea = linea.split(' +++$+++ ')
+    if len(_linea) == 5:
+        id2line[_linea[0]] = _linea[4]
+
+
+# Creazione di una lista con tutti gli ID delle linee di conversazione
+convs = [ ]
+for linea in linee_conversazioni[:-1]:
+    _linea = linea.split(' +++$+++ ')[-1][1:-1].replace("'","").replace(" ","")
+    convs.append(_linea.split(','))
+
+
+# Creazione liste da popolare
+lista_domande = []
+lista_risposte = []
+
+# Divisione e ordinamento delle frasi in nelle due liste di domande (input) e risposte (output)
+for conv in convs:
+    for i in range(len(conv)-1):
+        lista_domande.append(id2line[conv[i]])
+        lista_risposte.append(id2line[conv[i+1]])
+
+# A questo punto dovremmo avere due liste, domande e risposte, contenenti ciascuna tutte le frasi del database
+
+# Pulizia del testo
+# Creazione liste per frasi ripulite, da popolare
+lista_domande_pulite = []
+lista_risposte_pulite = []    
+
+# Cicli for che prendono in ingresso la linee di testo de lista_domande e lista_risposte,
+# le danno in pasto alla funzione pulizia_testo e popolano le liste_domande/risposte_pulite
+for domanda in lista_domande:
+    lista_domande_pulite.append(pulizia_testo(domanda))
+        
+for risposta in lista_risposte:
+    lista_risposte_pulite.append(pulizia_testo(risposta))
+
+# ====
+# per non avere q&a composti da frasi troppo lunghe, o domanda e risposta rispettivamte troppo
+# o troppo poco lughi, è bene fare una cernita 'incrociata'
+
+# Impostiano una lunghezza minima ed una massima del numero di parole che q&a può avere
+min_lunghezza_linea = 2
+max_lunghezza_linea = 15
+
+# Filtriamo le domande che sono troppo lunghe/corte
+domande_brevi_temp : list = []
+risposte_brevi_temp : list = []
+
+i = 0
+for domanda in lista_domande_pulite:
+    if len(domanda.split()) >= min_lunghezza_linea and len(domanda.split()) <= max_lunghezza_linea:
+        domande_brevi_temp.append(domanda)
+        risposte_brevi_temp.append(lista_risposte_pulite[i])
+    i += 1
+
+# Filtro delle risposte che sono troppo lunghe/brevi
+lista_domande_brevi = []
+lista_risposte_brevi = []
+
+i = 0
+for risposta in risposte_brevi_temp:
+    if len(risposta.split()) >= min_lunghezza_linea and len(risposta.split()) <= max_lunghezza_linea:
+        lista_risposte_brevi.append(risposta)
+        lista_domande_brevi.append(domande_brevi_temp[i])
+    i += 1
+    
+
+# Creazione oggetto chatbot tramite la libreria chatterbot
+condizioni_uscita = ("!exit", "!quit", "!q")
+
+chatbot = CB("HALL8000")
+#trainer = ListTrainer(chatbot)
+
+trainer = ChatterBotCorpusTrainer(chatbot)
+#trainer.train(lista_domande_brevi)
+trainer.train(
+    "chatterbot.corpus.english"
+)
+
+while True:
+    query = input("[TU]   > ")
+    if query in condizioni_uscita:
+        break
+    else:
+        print(f"[HALL] > {chatbot.get_response(query)}")
---
+++ p2/hall8001.py
@@ -0,0 +1,108 @@
+# Attenzione: versione di chatbot richiesta: 1.0.4
+# Attenzione: versione di python richiesta: 3.7.0
+
+# Importazione librerie:
+import re
+import time
+from chatterbot import ChatBot as CB
+from chatterbot.trainers import ListTrainer, ChatterBotCorpusTrainer
+
+#==============================================================
+
+# Definizioe funzione di pulizia del testo
+def pulizia_testo(testo):
+
+    # Impostazione testo in minuscolo
+    testo = testo.lower()
+    
+    testo = re.sub(r"i'm", "i am", testo)
+    testo = re.sub(r"he's", "he is", testo)
+    testo = re.sub(r"she's", "she is", testo)
+    testo = re.sub(r"it's", "it is", testo)
+    testo = re.sub(r"that's", "that is", testo)
+    testo = re.sub(r"what's", "that is", testo)
+    testo = re.sub(r"where's", "where is", testo)
+    testo = re.sub(r"how's", "how is", testo)
+    testo = re.sub(r"\'ll", " will", testo)
+    testo = re.sub(r"\'ve", " have", testo)
+    testo = re.sub(r"\'re", " are", testo)
+    testo = re.sub(r"\'d", " would", testo)
+    testo = re.sub(r"\'re", " are", testo)
+    testo = re.sub(r"won't", "will not", testo)
+    testo = re.sub(r"can't", "cannot", testo)
+    testo = re.sub(r"n't", " not", testo)
+    testo = re.sub(r"n'", "ng", testo)
+    testo = re.sub(r"'bout", "about", testo)
+    testo = re.sub(r"'til", "until", testo)
+    testo = re.sub(r"[-()\"#/@;:<>{}`+=~|.!?,]", "", testo)
+    
+    return testo
+
+# ============================================================
+
+
+linee = open('full_dialog.txt', encoding='utf-8', errors='ignore').read().split('\n')           # Caricamento file di testo
+
+id2line : dict = {}                                         # Creazione di un dizionario che mappa l'ID di ogni linea di dialogo con il proprio testo
+lista_linee : list = []
+
+id_conversazione = ''
+
+for linea in linee:
+    _linea = linea.split('\t')
+    if id_conversazione != _linea[2]:                       # controllo che l'ID della conversazione non sia cambiato
+        id_conversazione = _linea[2]                        # se l'id è cambiato, aggiornamento del nuovo controllo con il nuovo ID
+        lista_linee : list = []                             # azzeramento della lista lista_linee
+        troppo_lunga = False
+
+    lunghezza_linea = len(_linea[1].split())
+    if lunghezza_linea <= 30:                                   # se la lunghezza della linea di testo è maggiore di un x numero di parole
+        if troppo_lunga == False:                               # controllo che non sia mai stata trovata una frase oltre il limite in questa key del dizionario
+            lista_linee.append(pulizia_testo(_linea[1]))        # pulizia testo a aggiunta alla lista lista_linee della linea di conversazione in esame
+            id2line[id_conversazione] = lista_linee             # aggiornamento della key del dizionario con la lista_linee aggiornata
+        else:
+            lista_linee : list = []                             # azzeramento lista
+            id2line[id_conversazione] = lista_linee 
+
+    else:
+        troppo_lunga = True                                 # set variabile: da qua in poi bisogna azzerare le linee precedenti e non fare scrivere nulla dopo; almeno fino al prossimo cambio di ID
+        lista_linee : list = []                             # azzeramento lista
+        id2line[id_conversazione] = lista_linee             # inserimento lista azzerata nella chiave del dizionario
+
+"""
+if len(_linea) == 3:
+        lista_linee.append(pulizia_testo(_linea[1]))        # pulizia testo a aggiunta alla lista lista_linee della linea di conversazione in esame
+        id2line[id_conversazione] = lista_linee             # aggiornamento della key del dizionario con la lista_linee aggiornata
+"""
+
+
+condizioni_uscita = ("!exit", "!quit", "!q")                # Creazione oggetto chatbot tramite la libreria chatterbot
+
+chatbot = CB("HALL8000")
+trainer = ListTrainer(chatbot)
+
+
+#lunghezza_id2line = len(id2line)
+#print(id2line)
+indice = 0
+lista_conversazione : list = []
+
+while indice < len(id2line):                                # ciclo che dura per tutta la lunghezza del dizionario
+    lista_conversazione = list(id2line.values())[indice]             
+    trainer.train(lista_conversazione)                      # train del bot
+    
+    indice += 1                                             # avanzamento indice (e quindi key) e conversazione nel dizionario
+    lista_conversazione : list = []                         # azzeramento lista, pronta per essere popolata e per nuovo training
+
+
+print('========== PROGETTO CHATBOT HALL8001 ==========')
+print('==========          /start/          ==========/n')
+while True:
+    query = input("[TU]   > ")
+    if query in condizioni_uscita:
+        break
+    else:
+        print(f"[HALL] > {chatbot.get_response(query)}")
+
+print('/n==========           /end/           ==========')
+print('========== PROGETTO CHATBOT HALL8001 ==========')
